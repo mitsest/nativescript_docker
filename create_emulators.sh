df4f36e03b9401714d741715f97c14b030432819
mitsest <kascrew1@gmail.com>
2017-12-25 18:20:36 +0200

    supporting more than one emulators with the same android system image version

--- create_emulators.sh
+++ create_emulators.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-echo "Updating sdk libraries"
+# echo "Updating sdk libraries"
 sdkmanager --verbose --update
 yes | sdkmanager --licenses
 
 for folder in avd_conf/*/; do
-		android_version=$(basename $folder)
-		printf "Downloading system image for emulator (android version: $android_version)\n"
-
-		avd_ini_file=`find "$folder" -name '*.ini' | grep -v 'config.ini'`
+		avd_name=$(basename $folder)
+		avd_ini_file=$(find $folder -name "$avd_name.ini")
 		avd_config_ini_file=`find "$folder" -name 'config.ini'`
+		android_version_grep=$(grep 'image.sysdir.1=' $avd_config_ini_file)
+		system_image_awk=$(echo $android_version_grep | awk -F/ '{ print $2 }' $NF)
+		android_version=$(echo $system_image_awk | cut -d'-' -f 2)
 
-		avd_name=$(basename "$avd_ini_file" .ini)
-
-		if sdkmanager "system-images;android-$android_version;google_apis_playstore;x86" > /dev/null 2>&1 ; then
+		printf "Downloading system image for emulator (android version: $android_version)\n"
+		if sdkmanager --verbose "system-images;android-$android_version;google_apis_playstore;x86" > /dev/null 2>&1 ; then
 				printf "Creating emulator...\n"
-				echo no | avdmanager create avd --force --name $avd_name --package "system-images;android-$android_version;google_apis_playstore;x86"
+				echo no | avdmanager create avd --force --name $avd_name --package "system-images;android-$android_version;google_apis_playstore;x86" -verbosetwi
 		else
 			printf "$android_version has no system image with bundled playstore. Fallbacking to one without. \n"
-			sdkmanager "system-images;android-$android_version;google_apis;x86"
+			sdkmanager --verbose "system-images;android-$android_version;google_apis;x86"
 			printf "Creating emulator...\n"
-			echo no | avdmanager create avd --force --name $avd_name --package "system-images;android-$android_version;google_apis;x86"
+			echo no | avdmanager create avd --force --name $avd_name --package "system-images;android-$android_version;google_apis;x86" -verbose
 		fi
 
 
